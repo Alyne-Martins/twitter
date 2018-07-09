e74314ff81ff7922102d3d01c246d58889ad84b2
Alyne Martins <martins.alyne@gmail.com>
2018-07-08 23:43:09 -0300

    Versão 0.0.3

--- assets/script.js
+++ assets/script.js
@@ -23,14 +23,29 @@ function twittar() {
 }
 
 function ableBtn() {
-	var valor = document.getElementById('message').value;
-	var counterCaracter = valor.length;
-	document.getElementById('counterMessage').innerHTML = 140 - counterCaracter;
+
 	var message = document.getElementById("message").value;
 
 	if (message.length >= 1) {
 		if (document.getElementById('buttonTwittar').disabled) document.getElementById('buttonTwittar').disabled = false;
 		button.setAttribute("class", "buttonAble");
 	}
+}
 
+function caracter() {
+	var valor = document.getElementById('message').value;
+	var counterCaracter = valor.length;
+	var caracter = document.getElementById('counterMessage').innerHTML = 140 - counterCaracter;
+	if (caracter <= 0) {
+		document.getElementById('buttonTwittar').disabled = true;
+		button.removeAttribute("class", "buttonAble");
+		document.getElementById('counterMessage').style.color = "#ff0000";
+	}
+	if (caracter > 0 && caracter <= 10) {
+		document.getElementById('counterMessage').style.color = "#ffa100";
+	}
+
+	if (caracter >= 11 && caracter <= 20) {
+		document.getElementById('counterMessage').style.color = "#1da1f2";
+	}
 }
